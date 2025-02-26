3d11e42e039b8c0c065797292330b53d7526359e
Alexis433 <alexis.manuel433@gmail.com>
2025-02-26 13:10:23 +0100

    exoo4

--- apptaquin/lib/Exercice6.dart
+++ apptaquin/lib/Exercice6.dart
@@ -1,10 +1,78 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
-class Exercice6 extends StatelessWidget{
+// ==============
+// Models
+// ==============
+
+math.Random random = new math.Random();
+
+class Tile {
+  Color color;
+
+  Tile(this.color);
+
+  Tile.randomColor() : color = Color.fromARGB(
+      255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
+}
+
+// ==============
+// Widgets
+// ==============
+
+class TileWidget extends StatelessWidget {
+  final Tile tile;
+
+  TileWidget(this.tile);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return this.coloredBox();
+  }
 
+  Widget coloredBox() {
+    return Container(
+        color: tile.color,
+        child: Padding(
+          padding: EdgeInsets.all(70.0),
+        ));
+  }
 }
+
+//void main() => runApp(new MaterialApp(home: PositionedTiles()));
+
+class Exercice6 extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => PositionedTilesState();
+}
+
+class PositionedTilesState extends State<Exercice6> {
+  /*List<Widget> tiles =
+      List<Widget>.generate(4, (index) => TileWidget(Tile.randomColor()));*/
+      List<List<Widget>> tiles = List.generate(
+  4,  // Nombre de listes internes
+  (rowIndex) => List.generate(
+    4,  // Nombre d'éléments par liste interne
+    (colIndex) => TileWidget(Tile.randomColor()),
+  ),
+);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Moving Tiles'),
+        centerTitle: true,
+      ),
+      body: Row(children: tiles),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.sentiment_very_satisfied), onPressed: swapTiles),
+    );
+  }
+
+  swapTiles() {
+    setState(() {
+      tiles.insert(1, tiles.removeAt(0));
+    });
+  }
 }
--- apptaquin/lib/main.dart
+++ apptaquin/lib/main.dart
@@ -1,15 +1,8 @@
+import 'dart:math' as math;
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'Exercice1.dart';
-import 'Exercice2.dart';
-import 'Exercice4.dart';
-import 'Exercice5.dart';
-import 'Exercice6.dart';
-import 'Exercice7.dart';
 
-
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -17,124 +10,527 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      title: 'TP2',
+      home: const HomeScreen(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  final List<Map<String, String>> exercises = const [
+    {'title': 'Exercice 1', 'subtitle': 'Show image'},
+    {'title': 'Exercice 2a', 'subtitle': 'Rotate & Scale image'},
+    {'title': 'Exercice 2b', 'subtitle': 'Animated Rotate & Scale image'},
+    {'title': 'Exercice 4', 'subtitle': 'Display a Tile'},
+    {'title': 'Exercice 5a', 'subtitle': 'Grid of Colored Boxes'},
+    {'title': 'Exercice 5b', 'subtitle': 'Fixed Grid of Cropped image'},
+    {'title': 'Exercice 5c', 'subtitle': 'Configurable Taquin Board'},
+    {'title': 'Exercice 6a', 'subtitle': 'Moving Tiles'},
+    {'title': 'Exercice 6b', 'subtitle': 'Moving Tiles in Grid'},
+    {'title': 'Exercice 7', 'subtitle': 'Taquin v1'},
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Directionality Example')),
-      body: ListView(
-  children: [
-    Card(
-      child: ListTile(
-        leading: Icon(Icons.access_alarm),
-        title: Text("Exercice1"),
-        subtitle: Text('Wake up early!'),
-        onTap: () {
-          Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => Exercice2(),
-          ),);
-          // Action when tapped
-        },
+      appBar: AppBar(
+        title: const Text('TP2'),
+        backgroundColor: Colors.blue,
       ),
+      body: ListView.builder(
+        itemCount: exercises.length,
+        itemBuilder: (context, index) {
+          final exercise = exercises[index];
+          return Card(
+            margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+            child: ListTile(
+              title: Text(exercise['title']!),
+              subtitle: Text(exercise['subtitle']!),
+              trailing: const Icon(Icons.play_arrow),
+              onTap: () {
+                Widget page;
+                switch (index) {
+                  case 0:
+                    page = const PageOne();
+                    break;
+                  case 1:
+                    page = PageTwo();
+                    break;
+                  case 2:
+                    page = PageThree();
+                    break;
+                  case 3:
+                    page = PageFour();
+                    break;
+                  case 4:
+                    page = const PageFive();
+                    break;
+                  case 5:
+                    page = const PageSix();
+                    break;
+                  case 6:
+                    page = const PageSeven();
+                    break;
+                  case 7:
+                    page = const PageEight();
+                    break;
+                  case 8:
+                    page = const PageNine();
+                    break;
+                  case 9:
+                    page = const PageTen();
+                    break;
 
-    ),
-    Card(
-      child: ListTile(
-        leading: Icon(Icons.settings),
-        title: Text("Exercice 2"),
-        subtitle: Text('App settings'),
-        onTap: () {
-          Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => Exercice2(),
-          ),);
-        },
 
-      ),
-    ),
-    
-    Card(
-      child: ListTile(
-        leading: Icon(Icons.settings),
-        title: Text("Exercice 4"),
-        subtitle: Text('App settings'),
-        onTap: () {
-          Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => Exercice4(),
-          ),);
+                  default:
+                    page = const PageOne();
+                }
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => page),
+                );
+              },
+            ),
+          );
         },
       ),
-    ),
-    Card(
-      child: ListTile(
-        leading: Icon(Icons.settings),
-        title: Text("Exercice 5"),
-        subtitle: Text('App settings'),
-        onTap: () {
-          Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => Exercice5(),
-          ),);
-        },
-      ),
-    ),
-    Card(
-      child: ListTile(
-        leading: Icon(Icons.settings),
-        title: Text("Exercice 6"),
-        subtitle: Text('App settings'),
-        onTap: () {
-          Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => Exercice6(),
-          ),);
-        },
-      ),
-    ),
-    Card(
-      child: ListTile(
-        leading: Icon(Icons.settings),
-        title: Text("Exercice 2"),
-        subtitle: Text('App settings'),
-        onTap: () {
-          Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => Exercice2(),
-          ),);
-        },
-      ),
-    ),
-    Card(
-      child: ListTile(
-        leading: Icon(Icons.settings),
-        title: Text("Exercice 7"),
-        subtitle: Text('App settings'),
-        onTap: () {
-          Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => Exercice7(),
-          ),);
-        },
-      ),
-    ),
-  ],
-)
-
     );
   }
 }
+
+// ----------------------
+// Définition des pages
+// ----------------------
+
+class PageOne extends StatelessWidget {
+  const PageOne({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page 1'),
+      ),
+      body: const Center(
+        child: Image(
+          image: NetworkImage('https://picsum.photos/512/1024')
+        ),
+      ),
+    );
+  }
+}
+
+
+class PageTwo extends StatefulWidget {
+  @override
+
+  PageTwoState createState() => PageTwoState();
+
+}
+
+
+class PageTwoState extends State<PageTwo> {
+  double _rotationZ = 0;
+  double _rotationX = 0;
+  double _scale = 1;
+  bool _flip = false;
+
+  @override
+  Widget build(BuildContext context) {
+        return Scaffold(
+      appBar: AppBar(
+        title: Text('TP2'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ClipRect(
+              child: Container(
+                child: Transform(
+                  alignment: Alignment.center, 
+                  transform: Matrix4.identity()
+                    ..rotateZ(2 * math.pi * _rotationZ)
+                    ..rotateX(2 * math.pi * _rotationX)
+                    ..scale(2 * _scale),
+                  child: Transform.flip(
+                    flipX: _flip,
+                    child: Image(
+                      image: NetworkImage('https://picsum.photos/512/1024'),
+                      width: 100,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('RotateX :'),
+                Slider(
+                  value: _rotationX,
+                  min: 0,
+                  max: 1,
+                  onChanged: (double value) {
+                    setState(() {
+                      _rotationX = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('RotateZ :'),
+                Slider(
+                  value: _rotationZ,
+                  min: 0,
+                  max: 1,
+                  onChanged: (double value) {
+                    setState(() {
+                      _rotationZ = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Mirror :'),
+                Checkbox(
+                  value: _flip,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      _flip = value ?? false;
+                    });
+                  },
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Scale :'),
+                Slider(
+                  value: _scale,
+                  min: 0,
+                  max: 2,
+                  onChanged: (double value) {
+                    setState(() {
+                      _scale = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PageThree extends StatefulWidget {
+  @override
+
+  PageThreeState createState() => PageThreeState();
+
+}
+
+
+class PageThreeState extends State<PageThree> {
+  double _rotationZ = 0;
+  double _rotationX = 0;
+  double _scale = 1;
+  bool _flip = false;
+
+  Timer? _timer;
+  int _ticks = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    const duration = Duration(milliseconds: 50);
+    _timer = Timer.periodic(duration, animate);
+  }
+   
+
+    void animate(Timer t) {
+    setState(() {
+      _rotationZ = (_rotationZ + 0.01) % 1.0;
+      _rotationX = (_rotationX + 0.005) % 1.0;
+      _scale = 1.0 + 0.5 * math.sin(_ticks * 0.1);
+    });
+    _ticks++;
+    if (_ticks > 200) {
+      t.cancel();
+    }
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TP2'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ClipRect(
+              child: Container(
+                child: Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.identity()
+                    ..rotateZ(2 * math.pi * _rotationZ)
+                    ..rotateX(2 * math.pi * _rotationX)
+                    ..scale(2 * _scale),
+                  child: Transform.flip(
+                    flipX: _flip,
+                    child: Image(
+                      image: const NetworkImage('https://picsum.photos/512/1024'),
+                      width: 100,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('RotateX :'),
+                Slider(
+                  value: _rotationX,
+                  min: 0,
+                  max: 1,
+                  onChanged: (double value) {
+                    setState(() {
+                      _rotationX = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('RotateZ :'),
+                Slider(
+                  value: _rotationZ,
+                  min: 0,
+                  max: 1,
+                  onChanged: (double value) {
+                    setState(() {
+                      _rotationZ = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Mirror :'),
+                Checkbox(
+                  value: _flip,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      _flip = value ?? false;
+                    });
+                  },
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Scale :'),
+                Slider(
+                  value: _scale,
+                  min: 0,
+                  max: 2,
+                  onChanged: (double value) {
+                    setState(() {
+                      _scale = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+class PageFour extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Display a Tile as a Cropped Image'),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Column(children: [
+        SizedBox(
+            width: 150.0,
+            height: 150.0,
+            child: Container(
+                margin: EdgeInsets.all(20.0),
+                child: this.createTileWidgetFrom(tile))),
+        Container(
+            height: 200,
+            child: Image.network('https://picsum.photos/512/1024',
+                fit: BoxFit.cover))
+      ])),
+    );
+  }
+
+  Widget createTileWidgetFrom(Tile tile) {
+    return InkWell(
+      child: tile.croppedImageTile(3),
+      onTap: () {
+        print("tapped on tile");
+      },
+    );
+  }
+}
+class PageFive extends StatelessWidget {
+  const PageFive({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TP2'),
+      ),
+      body: Center(
+        child: Text('Page 5')
+      ),
+    );
+  }
+}
+
+class PageSix extends StatelessWidget {
+  const PageSix({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TP2'),
+      ),
+      body: Center(
+        child: Text('Page 6')
+      ),
+    );
+  }
+}
+
+class PageSeven extends StatelessWidget {
+  const PageSeven({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TP2'),
+      ),
+      body: Center(
+        child: Text('Page 7')
+      ),
+    );
+  }
+}
+
+class PageEight extends StatelessWidget {
+  const PageEight({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TP2'),
+      ),
+      body: Center(
+        child: Text('Page 8')
+      ),
+    );
+  }
+}
+
+class PageNine extends StatelessWidget {
+  const PageNine({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TP2'),
+      ),
+      body: Center(
+        child: Text('Page 9')
+      ),
+    );
+  }
+}
+
+class PageTen extends StatelessWidget {
+  const PageTen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TP2'),
+      ),
+      body: Center(
+        child: Text('Page 10')
+      ),
+    );
+  }
+}
+
+
+class Tile {
+  String imageURL;
+  Alignment alignment;
+
+  Tile({required this.imageURL,required this.alignment});
+
+  Widget croppedImageTile(gridSize) {
+    return FittedBox(
+      fit: BoxFit.fill,
+      child: ClipRect(
+        child: Container(
+          child: Align(
+            alignment: this.alignment,
+            widthFactor: 1/gridSize,
+            heightFactor: 1/gridSize,
+            child: Image.network(this.imageURL),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Tile tile = new Tile(
+    imageURL: 'https://picsum.photos/512/1024', alignment: Alignment(0, 0));
+
